b89b6ab85a88d25a7d57767e3271887fa5306ba0
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2023-07-06 16:38:08 +0530

    refactor: `Checkbox`

--- components/ui/checkbox.tsx
+++ components/ui/checkbox.tsx
@@ -6,53 +6,82 @@ import { Check } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 import { Label } from "./label";
+import { cva, VariantProps } from "class-variance-authority";
+
+const checkboxVariants = cva(
+  "peer shrink-0 rounded-[5px] border border-gray-300 hover:border-gray-400 focus:border-primary-300 focus:ring-2 focus:ring-[#EAF0FF] focus:ring-offset-2 focus:ring-offset-[#EAF0FF] focus-visible:outline-none disabled:cursor-not-allowed disabled:border-gray-200 disabled:bg-gray-100 data-checked:border-primary-500 data-checked:bg-primary-50 data-checked:text-primary-500 data-checked:focus:border-primary-500 data-checked:focus:bg-primary-500 data-checked:focus:text-white data-checked:disabled:border-gray-200 data-checked:disabled:bg-gray-100 data-checked:disabled:text-gray-200",
+  {
+    variants: {
+      size: {
+        sm: "h-4 w-4",
+        md: "h-5 w-5",
+      },
+    },
+    defaultVariants: {
+      size: "sm",
+    },
+  }
+);
+
+const iconVariants = cva("stroke-[3px]", {
+  variants: {
+    size: {
+      sm: "h-3 w-3",
+      md: "h-3.5 w-3.5",
+    },
+  },
+  defaultVariants: {
+    size: "sm",
+  },
+});
+
+const labelVariants = cva("text-gray-700 peer-disabled:text-gray-300", {
+  variants: {
+    text: {
+      sm: "text-sm",
+      md: "text-base",
+    },
+  },
+  defaultVariants: {
+    text: "sm",
+  },
+});
 
 interface CheckboxProps
-  extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root> {
-  label: string;
-  category?: string;
+  extends React.ComponentPropsWithoutRef<typeof CheckboxPrimitive.Root>,
+    VariantProps<typeof checkboxVariants> {
+  label?: string;
   desc?: string;
 }
 
 const Checkbox = React.forwardRef<
   React.ElementRef<typeof CheckboxPrimitive.Root>,
   CheckboxProps
->(({ className, label, desc, category, ...props }, ref) => {
+>(({ className, label, desc, size, ...props }, ref) => {
   const id = React.useId();
 
   return (
     <div className="flex gap-x-2">
       <CheckboxPrimitive.Root
-        ref={ref}
-        className={cn(
-          "peer h-4 w-4 shrink-0 rounded-[5px] border border-gray-300 hover:border-gray-400 focus-visible:outline-none disabled:cursor-not-allowed disabled:opacity-50 data-[state=checked]:border-primary-500 data-[state=checked]:bg-primary-500 data-[state=checked]:text-white data-[state=checked]:hover:border-[#2B61E5] data-[state=checked]:hover:bg-[#2B61E5]",
-          className
-        )}
-        {...props}
+        className={cn(checkboxVariants({ size, className }))}
         id={id}
+        {...props}
+        ref={ref}
       >
         <CheckboxPrimitive.Indicator
           className={cn("flex items-center justify-center text-current")}
         >
-          <Check className="h-3 w-3" />
+          <Check className={iconVariants({ size })} />
         </CheckboxPrimitive.Indicator>
       </CheckboxPrimitive.Root>
-      <Label className="text-sm text-gray-700" htmlFor={id}>
-        {category ? (
-          <div className="flex items-center gap-x-1.5">
-            <span>{label}</span>
-            <span className="inline-block h-1 w-1 shrink-0 rounded-full bg-gray-700"></span>
-            <span>{category}</span>
-          </div>
-        ) : (
-          label
-        )}
-        {desc && (
-          <span className="block text-sm font-normal text-gray-500">
-            {desc}
-          </span>
-        )}
-      </Label>
+      {label && (
+        <Label className={labelVariants({ text: size })} htmlFor={id}>
+          {label}
+          {desc && (
+            <span className="block font-normal text-gray-500">{desc}</span>
+          )}
+        </Label>
+      )}
     </div>
   );
 });
--- stories/Checkbox.stories.tsx
+++ stories/Checkbox.stories.tsx
@@ -7,12 +7,31 @@ const meta: Meta = {
     label: {
       control: "text",
     },
-    category: {
-      control: "text",
-    },
     desc: {
       control: "text",
     },
+    size: {
+      control: "select",
+      options: ["sm", "md"],
+    },
+    disabled: {
+      control: "boolean",
+    },
+  },
+  args: {
+    disabled: false,
+  },
+  parameters: {
+    design: [
+      {
+        type: "figma",
+        url: "https://www.figma.com/file/CgaxYAFROXbQH5fgPw8sX4/Blend-Metrics---New-Design-System?type=design&node-id=167-175850&mode=design&t=Oc4szCQ8jKnSmE0q-4",
+      },
+      {
+        type: "figma",
+        url: "https://www.figma.com/file/CgaxYAFROXbQH5fgPw8sX4/Blend-Metrics---New-Design-System?type=design&node-id=164-175145&mode=design&t=Oc4szCQ8jKnSmE0q-4",
+      },
+    ],
   },
 };
 
@@ -20,9 +39,16 @@ type Story = StoryObj<typeof Checkbox>;
 
 export default meta;
 
+export const Default: Story = {
+  args: {
+    size: "sm",
+  },
+};
+
 export const WithLabel: Story = {
   args: {
-    label: "Option 1",
+    ...Default.args,
+    label: "Remember me",
   },
   parameters: {
     design: {
@@ -32,23 +58,10 @@ export const WithLabel: Story = {
   },
 };
 
-export const WithCategory: Story = {
-  args: {
-    ...WithLabel.args,
-    category: "Personal",
-  },
-  parameters: {
-    design: {
-      type: "figma",
-      url: "https://www.figma.com/file/wTW5aoJfEb9KtPeU8KkbG3/BM---Integration-Partner---Development?type=design&node-id=3-99397&mode=design&t=t0Dj8nna877HiUD1-4",
-    },
-  },
-};
-
 export const WithDesc: Story = {
   args: {
-    ...WithCategory.args,
-    desc: "Video streaming service",
+    ...WithLabel.args,
+    desc: "Save my login details for next time.",
   },
   parameters: {
     design: {

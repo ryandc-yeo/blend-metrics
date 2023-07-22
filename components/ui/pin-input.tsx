989d17fae71a7a1b8056148d51d6d89f04bc2642
Dheeraj Singh Nagdali <dheerajsinghnagdali@gmail.com>
2023-07-22 15:03:43 +0530

    feat: \`PinInput\`'s stories & make it reusable

--- components/ui/pin-input.tsx
+++ components/ui/pin-input.tsx
@@ -3,91 +3,141 @@
 import * as React from "react";
 import * as pinInput from "@zag-js/pin-input";
 import { useMachine, normalizeProps } from "@zag-js/react";
+import { VariantProps, cva } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
 
+const pinInputVariants = cva(
+  "rounded-lg border border-gray-300 p-2 text-center font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50",
+  {
+    variants: {
+      size: {
+        sm: "h-16 w-16 text-5xl",
+        md: "h-20 w-20 text-5xl",
+        lg: "h-24 w-24 text-[60px] leading-none",
+      },
+    },
+    defaultVariants: {
+      size: "sm",
+    },
+  }
+);
+
+interface FourDigitPinInputProps
+  extends React.HTMLAttributes<HTMLDivElement>,
+    VariantProps<typeof pinInputVariants> {
+  placeholder?: string;
+  pinInputClassName?: string;
+}
+
 const FourDigitPinInput = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { placeholder?: string }
->(({ className, placeholder = "0", ...props }, ref) => {
-  const id = React.useId();
-  const [state, send] = useMachine(pinInput.machine({ id, placeholder }));
+  FourDigitPinInputProps
+>(
+  (
+    { className, placeholder = "0", size, pinInputClassName, ...props },
+    ref
+  ) => {
+    const id = React.useId();
+    const [state, send] = useMachine(pinInput.machine({ id, placeholder }));
 
-  const api = pinInput.connect(state, send, normalizeProps);
+    const api = pinInput.connect(state, send, normalizeProps);
 
-  return (
-    <div
-      className={cn("flex gap-x-2", className)}
-      {...props}
-      {...api.rootProps}
-      ref={ref}
-    >
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 0 })}
-      />
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 1 })}
-      />
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 2 })}
-      />
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 3 })}
-      />
-    </div>
-  );
-});
+    const _pinInputClassName = cn(
+      pinInputVariants({ size, className: pinInputClassName })
+    );
+
+    return (
+      <div
+        className={cn("flex items-center gap-x-2", className)}
+        {...props}
+        {...api.rootProps}
+        ref={ref}
+      >
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 0 })}
+        />
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 1 })}
+        />
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 2 })}
+        />
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 3 })}
+        />
+      </div>
+    );
+  }
+);
 
 FourDigitPinInput.displayName = "FourDigitPinInput";
 
+interface SixDigitPinInputProps
+  extends React.HTMLAttributes<HTMLElement>,
+    VariantProps<typeof pinInputVariants> {
+  placeholder?: string;
+  pinInputClassName?: string;
+}
+
 const SixDigitPinInput = React.forwardRef<
   HTMLDivElement,
-  React.HTMLAttributes<HTMLDivElement> & { placeholder?: string }
->(({ className, placeholder = "0", ...props }, ref) => {
-  const id = React.useId();
-  const [state, send] = useMachine(pinInput.machine({ id, placeholder }));
+  SixDigitPinInputProps
+>(
+  (
+    { className, placeholder = "0", size, pinInputClassName, ...props },
+    ref
+  ) => {
+    const id = React.useId();
+    const [state, send] = useMachine(pinInput.machine({ id, placeholder }));
 
-  const api = pinInput.connect(state, send, normalizeProps);
+    const api = pinInput.connect(state, send, normalizeProps);
 
-  return (
-    <div
-      className={cn("flex gap-x-2", className)}
-      {...props}
-      {...api.rootProps}
-      ref={ref}
-    >
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 0 })}
-      />
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 1 })}
-      />
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 2 })}
-      />
-      <span className="text-6xl font-medium text-gray-300">-</span>
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 3 })}
-      />
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 4 })}
-      />
-      <input
-        className="h-16 w-16 rounded-lg border border-gray-300 p-2 text-center  text-5xl font-medium text-gray-300 shadow-xs placeholder:text-gray-300 focus:border-primary-500 focus:ring-primary-500 disabled:opacity-50"
-        {...api.getInputProps({ index: 5 })}
-      />
-    </div>
-  );
-});
+    const _pinInputClassName = pinInputVariants({
+      size,
+      className: pinInputClassName,
+    });
+
+    return (
+      <div
+        className={cn("flex items-center gap-x-2", className)}
+        {...props}
+        {...api.rootProps}
+        ref={ref}
+      >
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 0 })}
+        />
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 1 })}
+        />
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 2 })}
+        />
+        <span className="text-6xl font-medium text-gray-300">-</span>
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 3 })}
+        />
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 4 })}
+        />
+        <input
+          className={_pinInputClassName}
+          {...api.getInputProps({ index: 5 })}
+        />
+      </div>
+    );
+  }
+);
 
 SixDigitPinInput.displayName = "SixDigitPinInput";
 
--- stories/pin-input.stories.tsx
+++ stories/pin-input.stories.tsx
@@ -1,9 +1,20 @@
 import { Meta, StoryObj } from "@storybook/react";
 
-import { FourDigitPinInput, SixDigitPinInput } from "@/components/ui";
+import {
+  FourDigitPinInput,
+  HelperText,
+  Label,
+  SixDigitPinInput,
+} from "@/components/ui";
 
 const meta: Meta = {
   title: "PinInput",
+  argTypes: {
+    size: {
+      control: "select",
+      options: ["sm", "md", "lg"],
+    },
+  },
   parameters: {
     design: {
       type: "figma",
@@ -20,4 +31,60 @@ export const FourDigit: StoryObj = {
 
 export const SixDigit: StoryObj = {
   render: (args) => <SixDigitPinInput {...args} />,
+};
+
+export const LabeledFourDigit: StoryObj = {
+  render: (args) => (
+    <div className="space-y-1.5">
+      <Label size="sm">Secure code</Label>
+      <FourDigitPinInput {...args} />
+    </div>
+  ),
+};
+
+export const LabeledHelperTextFourDigit: StoryObj = {
+  render: (args) => (
+    <div className="space-y-1.5">
+      <Label size="sm">Secure code</Label>
+      <FourDigitPinInput {...args} />
+      <HelperText size="sm">This is a hint text to help user.</HelperText>
+    </div>
+  ),
+};
+
+export const HelperTextFourDigit: StoryObj = {
+  render: (args) => (
+    <div className="space-y-1.5">
+      <FourDigitPinInput {...args} />
+      <HelperText size="sm">This is a hint text to help user.</HelperText>
+    </div>
+  ),
+};
+
+export const LabeledSixDigitPinInput: StoryObj = {
+  render: (args) => (
+    <div className="space-y-1.5">
+      <Label size="sm">Secure code</Label>
+      <SixDigitPinInput {...args} />
+    </div>
+  ),
+};
+
+export const LabeledHelperTextSixDigitPinInput: StoryObj = {
+  render: (args) => (
+    <div className="space-y-1.5">
+      <Label size="sm">Secure code</Label>
+      <SixDigitPinInput {...args} />
+      <HelperText size="sm">This is a hint text to help user.</HelperText>
+    </div>
+  ),
+};
+
+export const HelperTextSixDigitPinInput: StoryObj = {
+  render: (args) => (
+    <div className="space-y-1.5">
+      <SixDigitPinInput {...args} />
+      <HelperText size="sm">This is a hint text to help user.</HelperText>
+    </div>
+  ),
 };

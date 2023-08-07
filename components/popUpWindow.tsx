d091e3887d2ed508189effd8b15983b2e919c61f
Ryan Yeo <ryandc.yeo@gmail.com>
2023-08-07 14:15:23 -0700

    removed autofocus on popUpWindow input field

--- components/popUpWindow.tsx
+++ components/popUpWindow.tsx
@@ -44,7 +44,7 @@ const PopUpWindow = () => {
           </Transition.Child>
 
           <div className="fixed inset-0 overflow-y-auto">
-            <div className="flex min-h-full items-center justify-center p-4 text-center">
+            <div className="flex min-h-full items-center justify-center p-4 text-left">
               <Transition.Child
                 as={Fragment}
                 enter="ease-out duration-300"

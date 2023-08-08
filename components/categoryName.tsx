341c4cda2b398fccb94c2d644dcf334650a8f5d5
Ryan Yeo <ryandc.yeo@gmail.com>
2023-08-08 12:31:11 -0700

    implemented radio buttons for popUpWindow sidebar

--- components/categoryName.tsx
+++ components/categoryName.tsx
@@ -1,29 +1,24 @@
-import Image from "next/image";
-import { CategoryNameBlueIcon, CategoryNameIcon } from "@/components/assets";
-import { useState } from "react";
+import {
+  CategoryNameBlueIcon,
+  CategoryNameIcon,
+  ViewAllIcon,
+  ViewAllBlueIcon,
+  RecommendedBlueIcon,
+  RecommendedIcon,
+  RecentlyViewedBlueIcon,
+  RecentlyViewedIcon,
+  MyCollectionBlueIcon,
+  MyCollectionIcon,
+} from "@/components/assets";
 
 export type Props = {
   variant?: true | false;
 };
 
-const CategoryName = ({
-  variant = false,
-  title = "Category Name",
-  icon = CategoryNameIcon,
-  iconBlue = CategoryNameBlueIcon,
-}) => {
-  const [active, setActive] = useState(variant);
-
-  const handleClick = () => {
-    setActive(!active);
-  };
-
-  if (active) {
+export const CategoryName = ({ variant = false, title = "Category Name" }) => {
+  if (variant) {
     return (
-      <button
-        onClick={handleClick}
-        className="flex w-56 max-w-full items-center rounded-md bg-[#EAF0FF] px-3 py-2"
-      >
+      <button className="flex w-56 max-w-full items-center rounded-md bg-[#EAF0FF] px-3 py-2">
         <CategoryNameBlueIcon />
         <p className="pl-3 text-sm font-semibold capitalize text-[#306CFE]">
           {title}
@@ -32,10 +27,7 @@ const CategoryName = ({
     );
   } else {
     return (
-      <button
-        onClick={handleClick}
-        className="flex w-56 max-w-full items-center rounded-md bg-white px-3 py-2 hover:bg-gray-100"
-      >
+      <button className="flex w-56 max-w-full items-center rounded-md bg-white px-3 py-2 hover:bg-gray-100">
         <CategoryNameIcon />
         <p className="pl-3 text-sm font-semibold capitalize text-[#667085]">
           {title}
@@ -45,4 +37,93 @@ const CategoryName = ({
   }
 };
 
-export default CategoryName;
+export const ViewAll = ({ variant = false, title = "View All" }) => {
+  if (variant) {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-[#EAF0FF] px-3 py-2">
+        <ViewAllBlueIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#306CFE]">
+          {title}
+        </p>
+      </button>
+    );
+  } else {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-white px-3 py-2 hover:bg-gray-100">
+        <ViewAllIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#667085]">
+          {title}
+        </p>
+      </button>
+    );
+  }
+};
+
+export const Recommended = ({ variant = false, title = "Recommended" }) => {
+  if (variant) {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-[#EAF0FF] px-3 py-2">
+        <RecommendedBlueIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#306CFE]">
+          {title}
+        </p>
+      </button>
+    );
+  } else {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-white px-3 py-2 hover:bg-gray-100">
+        <RecommendedIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#667085]">
+          {title}
+        </p>
+      </button>
+    );
+  }
+};
+
+export const RecentlyViewed = ({
+  variant = false,
+  title = "RecentlyViewed",
+}) => {
+  if (variant) {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-[#EAF0FF] px-3 py-2">
+        <RecentlyViewedBlueIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#306CFE]">
+          {title}
+        </p>
+      </button>
+    );
+  } else {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-white px-3 py-2 hover:bg-gray-100">
+        <RecentlyViewedIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#667085]">
+          {title}
+        </p>
+      </button>
+    );
+  }
+};
+
+export const MyCollection = ({ variant = false, title = "My Collection" }) => {
+  if (variant) {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-[#EAF0FF] px-3 py-2">
+        <MyCollectionBlueIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#306CFE]">
+          {title}
+        </p>
+      </button>
+    );
+  } else {
+    return (
+      <button className="flex w-56 max-w-full items-center rounded-md bg-white px-3 py-2 hover:bg-gray-100">
+        <MyCollectionIcon />
+        <p className="pl-3 text-sm font-semibold capitalize text-[#667085]">
+          {title}
+        </p>
+      </button>
+    );
+  }
+};
--- components/workflowTemplate_sidebar.tsx
+++ components/workflowTemplate_sidebar.tsx
@@ -1,6 +1,12 @@
 "use client";
 
-import CategoryName from "./categoryName";
+import {
+  CategoryName,
+  ViewAll,
+  Recommended,
+  RecentlyViewed,
+  MyCollection,
+} from "./categoryName";
 import * as ScrollArea from "@radix-ui/react-scroll-area";
 import {
   ViewMarketplaceIcon,
@@ -14,56 +20,85 @@ import {
   MyCollectionBlueIcon,
 } from "@/components/assets";
 import { useState } from "react";
-
-const TAGS = Array.from({ length: 10 }).map(
-  (_, i, a) => `Category Name ${a.length - i}`
-);
+import { RadioGroup } from "@headlessui/react";
 
 const WorkflowTemplate_Sidebar = () => {
+  const [option, setOption] = useState(1);
+
   return (
     <div className="relative h-[635px] w-56 overflow-hidden rounded-bl-xl bg-white pb-[5px] pl-[15px]">
       <ScrollArea.Root className="h-[91%] w-full overflow-hidden bg-white pt-[15px]">
         <ScrollArea.Viewport className="h-full w-full">
-          <div className="flex w-[87%] flex-col gap-6">
-            <div className="flex w-full flex-col gap-2 self-stretch">
-              <h3 className="text-xs font-medium uppercase leading-5 tracking-[0.24px] text-gray-500">
-                for you
-              </h3>
-              <div className="flex flex-col gap-1">
-                <CategoryName
-                  title="view all"
-                  icon={ViewAllIcon}
-                  iconBlue={ViewAllBlueIcon}
-                  variant={true}
-                />
-                <CategoryName
-                  title="recommended"
-                  icon={RecommendedIcon}
-                  iconBlue={RecommendedBlueIcon}
-                />
-                <CategoryName
-                  title="recently viewed"
-                  icon={RecentlyViewedIcon}
-                  iconBlue={RecentlyViewedBlueIcon}
-                />
-                <CategoryName
-                  title="my collection"
-                  icon={MyCollectionIcon}
-                  iconBlue={MyCollectionBlueIcon}
-                />
+          <RadioGroup value={option} onChange={setOption} defaultValue={1}>
+            <div className="flex w-[87%] flex-col gap-6">
+              <div className="flex w-full flex-col gap-2 self-stretch">
+                <h3 className="text-xs font-medium uppercase leading-5 tracking-[0.24px] text-gray-500">
+                  for you
+                </h3>
+                <div className="flex flex-col gap-1">
+                  <RadioGroup.Option value={1}>
+                    {({ checked }) => <ViewAll variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={2}>
+                    {({ checked }) => <Recommended variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={3}>
+                    {({ checked }) => <RecentlyViewed variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={4}>
+                    {({ checked }) => <MyCollection variant={checked} />}
+                  </RadioGroup.Option>
+                </div>
+              </div>
+              <div className="flex flex-col gap-2">
+                <h3 className="text-xs font-medium uppercase leading-5 tracking-[0.24px] text-gray-500">
+                  browse by category
+                </h3>
+                <div className="flex flex-col gap-1">
+                  <RadioGroup.Option value={5}>
+                    {({ checked }) => (
+                      <CategoryName variant={checked} title="Category 1" />
+                    )}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={6}>
+                    {({ checked }) => (
+                      <CategoryName variant={checked} title="Category 2" />
+                    )}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={7}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={8}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={9}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={10}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={11}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={12}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={13}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={14}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={15}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                  <RadioGroup.Option value={16}>
+                    {({ checked }) => <CategoryName variant={checked} />}
+                  </RadioGroup.Option>
+                </div>
               </div>
             </div>
-            <div className="flex flex-col gap-2">
-              <h3 className="text-xs font-medium uppercase leading-5 tracking-[0.24px] text-gray-500">
-                browse by category
-              </h3>
-              {TAGS.map((tag) => (
-                <div className="flex flex-col gap-1" key={tag}>
-                  <CategoryName title={tag} />
-                </div>
-              ))}
-            </div>
-          </div>
+          </RadioGroup>
         </ScrollArea.Viewport>
         <ScrollArea.Scrollbar
           className="duration-[160ms] absolute right-[10px] flex touch-none select-none bg-white px-1 py-[3px] transition-colors ease-out data-[orientation=horizontal]:h-2.5 data-[orientation=vertical]:w-[13px] data-[orientation=horizontal]:flex-col"
@@ -74,11 +109,10 @@ const WorkflowTemplate_Sidebar = () => {
       </ScrollArea.Root>
       <div className="absolute bottom-[15px] h-fit w-fit hover:cursor-pointer">
         <a className="flex gap-2 text-[#306CFE]">
-          <p className="text-center text-sm font-semibold capitalize leading-5">
+          <p className="text-center text-sm font-semibold capitalize leading-5 hover:underline">
             View Marketplace
           </p>
           <ViewMarketplaceIcon />
-          {/* <Image src={ViewMarketplaceIcon} alt="share icon" /> */}
         </a>
       </div>
     </div>
